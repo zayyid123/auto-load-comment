f895d90dc4fe977c28552e60563969f67d4d574e
zayyid <mochamadzayyid@gmail.com>
2022-03-30 14:14:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [items, setitems] = React.useState([])
+
+  const [page, setpage] = React.useState(2)
+
+  React.useEffect(() => {
+    const getMyData = async() => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+
+      const data = await res.json()
+      setitems(data)
+    }
+
+    getMyData()
+  }, [])
+
+  const getData = async() => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${page}/comments`)
+
+    const data = await res.json()
+    return data
+  }
+
+  const fetchData = async() => {
+    const dataFromServer = await getData();
+
+    setitems([...items, ...dataFromServer])
+
+    setpage(page+1)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      textAlign: 'center'
+    }}>
+      <h1>Infinite Scroll With jsonplaceholder</h1>
+
+      <p>You can find all of this code in my github: zayyid123</p>
+
+      <InfiniteScroll
+        dataLength={items.length} 
+        next={fetchData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        
+      >
+        {
+          items.map((item, index) =>
+            <div key={index} 
+              style={{
+                margin: 'auto',
+                padding: '15px',
+                boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+                borderRadius: '10px',
+                maxWidth: '350px',
+                marginTop: '10px'
+                }}>
+              <h3>{item.name.toUpperCase()}</h3>
+              <h4>{item.email}</h4>
+              <p>{item.body}</p>
+            </div>
+          )
+        }
+      </InfiniteScroll>
     </div>
   );
 }
